79a483731d45080b84847987e8d6e04a734fe4ff
dingdangmaoup <dingdangmaoup@gmail.com>
2024-10-15 11:24:47 +0800

    Refactor HeadPanel component and add icons for add and user settings

--- src/components/HeadPanel/index.tsx
+++ src/components/HeadPanel/index.tsx
@@ -1,6 +1,30 @@
+import FluentAdd16Regular from '~icons/fluent/add-16-regular?width=16px&height=16px';
+import LucideUserCog from '~icons/lucide/user-cog?width=16px&height=16px';
 const HeadPanel:React.FC = (props) => {
   return (
-    <div {...props}>HeadPanel</div>
+    <div {...props}> 
+     <div className="flex flex-row justify-between">
+    <div className="flex flex-row items-center">
+      <IconLocalLogo className="w-6 h-6" />
+      <div className="text-lg font-semibold ml-2">OutSide</div>
+    </div>
+    <div className="flex flex-row items-center space-x-5">
+      <FluentAdd16Regular className="w-6 h-6 cursor-pointer hover:bg-blueGray" />
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <LucideUserCog className="w-5 h-5  cursor-pointer hover:bg-blueGray" />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  </div></div>
   )
 }
 
