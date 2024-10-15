38aae6622d4a34702aa526db811646bf898f9e2e
dingdangmaoup <dingdangmaoup@gmail.com>
2024-10-15 13:34:02 +0800

    Refactor HeadPanel component and add settings icon

--- src/components/HeadPanel/index.tsx
+++ src/components/HeadPanel/index.tsx
@@ -1,30 +1,35 @@
-import FluentAdd16Regular from '~icons/fluent/add-16-regular?width=16px&height=16px';
-import LucideUserCog from '~icons/lucide/user-cog?width=16px&height=16px';
-const HeadPanel:React.FC = (props) => {
+import FluentAdd16Regular from '~icons/fluent/add-16-regular';
+import LucideUserCog from '~icons/lucide/user-cog';
+import FluentSettings16Regular from '~icons/fluent/settings-16-regular';
+const HeadPanel: React.FC = () => {
   return (
-    <div {...props}> 
-     <div className="flex flex-row justify-between">
-    <div className="flex flex-row items-center">
-      <IconLocalLogo className="w-6 h-6" />
-      <div className="text-lg font-semibold ml-2">OutSide</div>
-    </div>
-    <div className="flex flex-row items-center space-x-5">
-      <FluentAdd16Regular className="w-6 h-6 cursor-pointer hover:bg-blueGray" />
-      <DropdownMenu>
-        <DropdownMenuTrigger>
-          <LucideUserCog className="w-5 h-5  cursor-pointer hover:bg-blueGray" />
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Profile</DropdownMenuItem>
-          <DropdownMenuItem>Billing</DropdownMenuItem>
-          <DropdownMenuItem>Team</DropdownMenuItem>
-          <DropdownMenuItem>Subscription</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </div>
-  </div></div>
+    <div>
+      <div className="flex flex-row justify-between">
+        <div className="flex flex-row items-center">
+          <IconLocalLogo className="w-6 h-6" />
+          <div className="text-lg font-semibold ml-2">OutSide</div>
+        </div>
+        <div className="flex flex-row items-center space-x-1 ">
+          <Button variant="ghost" size='sm' >
+            <FluentAdd16Regular className="w-6 h-6 " />
+          </Button>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" size='sm'  >
+                <LucideUserCog className="w-6 h-6 " />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Billing</DropdownMenuItem>
+              <DropdownMenuItem>Team</DropdownMenuItem>
+              <DropdownMenuItem><FluentSettings16Regular/>设置</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div></div>
   )
 }
 
