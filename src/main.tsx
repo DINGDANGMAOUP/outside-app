598fd77102405cd6f25ebd1e9122f299fa1b80e8
dingdangmaoup <dingdangmaoup@gmail.com>
2024-10-18 08:54:30 +0800

    Refactor main.tsx to fix import path for main.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import '@/styles/main.css'
 import '@/utils/i18n'
 import { RouterProvider } from "react-router-dom";
+import './styles/main.css'
 import router from "./route";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
